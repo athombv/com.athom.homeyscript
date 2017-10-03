07b71ee2020571c87b5ba9b8a2f3100c2d7aef92
Joachim Roeleveld <joachimroeleveld@gmail.com>
2017-10-03 14:31:07 +0200

    Homey API methods camelCase

--- www/js/homeyscript.sidebar.js
+++ www/js/homeyscript.sidebar.js
@@ -1,21 +1,21 @@
 function Sidebar( api, editor ) {
-	
+
 	this._scripts = [];
 	this._activeFileEl = undefined;
 	this._ctxmenuEl = undefined;
-	
+
 	this._api = api;
 	this._app = null;
 	this._editor = editor;
-	
+
 	this._api.registerAppListener( this._onApp.bind(this) );
-	
+
 	this._sidebarEl = document.querySelector('.hs-sidebar');
 	this._filesEl = this._sidebarEl.querySelector('.hs-files');
 	this._actionsEl = this._sidebarEl.querySelector('.hs-actions');
 	this._buttonCreateEl = this._sidebarEl.querySelector('.hs-actions .button.create');
 	this._buttonCreateEl.addEventListener('click', this.createScript.bind(this));
-	
+
 }
 
 Sidebar.prototype.init = function(){
@@ -23,30 +23,30 @@ Sidebar.prototype.init = function(){
 }
 
 Sidebar.prototype.render = function(){
-	
+
 	this._filesEl.innerHTML = '';
-	
+
 	this._scripts.forEach(function(scriptId){
 		var fileEl = document.createElement('div');
 			fileEl.classList.add('hs-file', 'hs-transition');
 			fileEl.addEventListener('click', function(e){
 				this._editor.open( scriptId );
-				
+
 				if( this._activeFileEl )
 					this._activeFileEl.classList.remove('hs-active');
-				
+
 				this._activeFileEl = fileEl;
 				this._activeFileEl.classList.add('hs-active');
 			}.bind(this));
 			fileEl.addEventListener('contextmenu', function(e){
 				e.preventDefault();
-				
+
 				this._destroyCtxmenu();
-					
+
 				var rect = this._filesEl.getBoundingClientRect();
-				
+
 				window.addEventListener('click', this._destroyCtxmenu.bind(this));
-								
+
 				this._ctxmenuEl = document.createElement('div');
 				this._ctxmenuEl.classList.add('ctxmenu');
 				this._ctxmenuEl.style.top = ( e.clientY - rect.top ) + 'px';
@@ -55,7 +55,7 @@ Sidebar.prototype.render = function(){
 					e.stopPropagation();
 				});
 				this._filesEl.appendChild(this._ctxmenuEl);
-				
+
 				var deleteFileEl = document.createElement('div');
 					deleteFileEl.classList.add('item');
 					deleteFileEl.textContent = 'Delete';
@@ -64,35 +64,35 @@ Sidebar.prototype.render = function(){
 						this._destroyCtxmenu();
 					}.bind(this));
 				this._ctxmenuEl.appendChild(deleteFileEl);
-				
+
 			}.bind(this));
-			
+
 		var fileIconEl = document.createElement('i');
 			fileIconEl.classList.add('fa', 'fa-file-o');
 		fileEl.appendChild(fileIconEl);
-			
+
 		var fileNameEl = document.createElement('span');
 			fileNameEl.textContent = scriptId;
 		fileEl.appendChild(fileNameEl);
-		
+
 		this._filesEl.appendChild(fileEl);
 	}.bind(this));
-	
+
 }
 
 Sidebar.prototype._destroyCtxmenu = function() {
-				
+
 	if( this._ctxmenuEl ) {
 		this._filesEl.removeChild(this._ctxmenuEl);
 		this._ctxmenuEl = null;
 	}
-	
+
 }
 
 Sidebar.prototype._onApp = function( app ) {
 	this._app = app;
 	this._refreshScripts();
-	
+
 	if( this._app ) {
 		this._actionsEl.style.visibility = 'visible';
 	} else {
@@ -101,12 +101,12 @@ Sidebar.prototype._onApp = function( app ) {
 }
 
 Sidebar.prototype._refreshScripts = function(){
-	
-	if( this._app ) {	
-		this._app.ApiGet('script')
+
+	if( this._app ) {
+		this._app.apiGet('script')
 			.then(function(scripts){
-				this._scripts = scripts;			
-				this.render();	
+				this._scripts = scripts;
+				this.render();
 			}.bind(this))
 			.catch(function(err){
 				console.error(err);
@@ -114,25 +114,25 @@ Sidebar.prototype._refreshScripts = function(){
 			})
 	} else {
 		this._scripts = [];
-		this.render();			
+		this.render();
 	}
-	
+
 }
 
 Sidebar.prototype.deleteScript = function( scriptId ) {
-	this._app.ApiDelete('script/' + scriptId)
+	this._app.apiDelete('script/' + scriptId)
 		.then(function(scripts){
 			this._refreshScripts();
 		}.bind(this))
 }
 
 Sidebar.prototype.createScript = function() {
-	
+
 	var scriptId = prompt("Please enter your script name.");
 	if( scriptId === null | scriptId.length < 1 ) return;
 	if( this._scripts.indexOf(scriptId) > -1 ) return alert("That name already exists. Please choose a unique name.");
-	
-	this._app.ApiPut('script/' + scriptId, { code: '// my script' })
+
+	this._app.apiPut('script/' + scriptId, { code: '// my script' })
 		.then(function(scripts){
 			this._refreshScripts();
 			this._editor.open( scriptId );
